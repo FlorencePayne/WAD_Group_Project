c9f4631de5dcb0ed8ba92b0955e8825878a06b8a
Parsa Tavakoli <2555801t@student.gla.ac.uk>
2022-03-14 17:47:04 +0000

    Created base.html for inheriting other pages.

--- floppa/views.py
+++ floppa/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from django.shortcuts import render
 def index(request):
-    return HttpResponse("Welcome To Floppabunny")
+    context_dict = {'boldmessage': 'Necklaces'}
+    return render(request, 'floppa/index.html', context=context_dict)
 
+def about(request):
+    return render(request, 'floppa/about.html')
