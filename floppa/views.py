4af7480c517af856e8b732692abef44ca8d2f59c
Bader Alabdullah <100683693+2675633A@users.noreply.github.com>
2022-03-19 13:43:58 +0000

    Update views.py

--- floppa/views.py
+++ floppa/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 def index(request):
     context_dict = {'boldmessage': 'Necklaces'}
-    return render(request, 'floppa/index.html', context=context_dict)
+    return render(request, 'floppa/index.html', context = context_dict)
 
 def about(request):
     return render(request, 'floppa/about.html')
